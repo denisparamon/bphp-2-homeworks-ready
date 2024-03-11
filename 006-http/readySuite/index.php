74ee47ab7c708a41def6f7b9bb7acb83ba723e8d
Hooman <denis_paramon@icloud.com>
2024-03-11 16:20:05 +0500

    editS

---
+++ 006-http/readySuite/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Список страниц</title>
+</head>
+<body>
+<h1>Список страниц</h1>
+<ul>
+    <li><a href="page_not_found.php">Страница не найдена</a></li>
+    <li><a href="save_file.php?text=Hello">Сохранить файл с текстом "Hello"</a></li>
+    <li><a href="session_start.php">Страница со счетчиком сессии</a></li>
+    <li><a href="session_counter.php">Страница с выводом счетчика сессии</a></li>
+</ul>
+</body>
+</html>
